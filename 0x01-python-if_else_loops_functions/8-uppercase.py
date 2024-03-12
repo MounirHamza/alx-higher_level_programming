322b0429311c97bcee3963a422b2ea0d202f1487
MounirHamza <mounir--hamza@outlook.com>
2024-03-12 14:49:35 -0700

    tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for char in str:
-        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-        print(uppercase_char, end="")
-    print()
+def uppercase(s):
+    result = ''
+    for x in range(0, len(s)):
+        char = s[x]
+        i = ord(char)
+        if i >= 97 and i < 123:
+            j = i - 32
+            result += chr(j)
+        else:
+            result += char
+    return print('{}'.format(result), end="\n")
