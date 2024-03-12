76813ed443b7e6d0dfc3303308defdca74d96d6d
MounirHamza <mounir--hamza@outlook.com>
2024-03-12 07:50:19 -0700

    tasks

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,5 +5,5 @@ if str(number)[-1]>'5':
     print(f'{number} and is greater than 5')
 elif str(number)[-1]=='0':
     print(f'{number} and is zero')
-elif str(number)[-1]<6 and!=0:
+else:
     print(f'{number} and is less than 6 and not 0')
