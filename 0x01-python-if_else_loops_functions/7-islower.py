f9fd5579c9d9ce3816579788b3e84f87d3a8b952
MounirHamza <mounir--hamza@outlook.com>
2024-03-12 11:25:58 -0700

    tasks

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,6 +2,6 @@
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
 if islower('c') == True:
-    print('{} is lower'.format('a', 'z'))
+    print('{} => lower'.format('a', 'z'))
 else:
-    print('{} is upper'.format('a', 'z'))
+    print('{} => upper'.format('a', 'z'))
