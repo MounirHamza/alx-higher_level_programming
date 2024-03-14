99ea9ce4b0eaf1fbb966a2767b7ac8499b241caf
MounieHamza <mounir--hamza@outlook.com>
2024-03-14 23:11:25 +0000

    tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     import sys
     argument = sys.argv[1:]
-    for i in range (len(argument)):
-        result = int[1:] + int[:-1]
+    int(x) for x in len(argument):
+        result = int(1:) + int[:-1]
         print('{}'.format(result))
