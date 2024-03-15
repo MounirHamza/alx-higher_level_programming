6465753d46b4b97cf57457a63cdb72097d300bc9
MounieHamza <mounir--hamza@outlook.com>
2024-03-15 00:40:22 +0000

    tasks

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import sys
-argument = sys.argv[1:]
-print('{}'.isalpha(argument))
+if __name__ == '__main__':
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != '__':
+            print(name)
