884a1020c6e4ca8e6e7c14fde48f4295cd11958e
MounieHamza <mounir--hamza@outlook.com>
2024-03-18 17:26:48 +0000

    tasks

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range my_list[0:]:
-    print("{}\n".format(my_list),end"")
+def print_list_integer(my_list=[]):
+    
+    for i in range my_list[0:]:
+        print("{}\n".format(my_list),end"")
