b4bebb4c3c70429aab3b363f2e7fca2f6bff5085
MounieHamza <mounir--hamza@outlook.com>
2024-03-19 02:15:59 +0000

    tasks

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    x = my_list[0]
+    for i in range (len(my_list)):
+        if my_list = "":
+            return 0
+        else my_list[i] > x:
+            x = my_list[i]
+            
+    return x
