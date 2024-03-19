4cbb3f0c0a679e38a3c180ef532120333f0db195
MounieHamza <mounir--hamza@outlook.com>
2024-03-19 01:19:12 +0000

    tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if sentence == "":
         return (0, "None")
     else:
-        return (len(new_tuple), new_tuple[0])
+        return (len(sentence), sentence[0])
